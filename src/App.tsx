6f07a906b287a3e9e89fd48c4c05b4c7485004f9
TheDiabolicalKid <marine.opsommer@gmail.com>
2026-01-13 16:50:12 +0100

    compléter home tsx et ajout home css

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
 
     </BrowserRouter>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
+import './Home.css';
+import { Link } from 'react-router-dom';
+
 function Home() {
     return (
         <div className="home">
-            <h1> Bienvenue sur mon Portfolio</h1>
-            <p> Page d'accueil </p>
+            <section className="hero">
+                <h1> Salut, je suis <span className="highlight"> Marine </span></h1>
+                <h2>Développeuse web Full Stack Junior</h2>
+                <p> Passionnée par le développement web et en constante apprentissage.</p>
+                <Link to="/project" className="cta-button">Voir mes projets</Link>
+            </section>
         </div>
     );
 }
